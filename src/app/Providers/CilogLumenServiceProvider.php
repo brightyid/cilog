41b5fc95424da10596bd0360af5592ee675c81d1
Egi Aprilianto <contact@asln.dev>
2024-10-07 14:21:30 +0700

    refactor: rename CilogServiceProvider to CilogLumenServiceProvider

--- src/app/Providers/CilogLumenServiceProvider.php
+++ src/app/Providers/CilogLumenServiceProvider.php
@@ -5,7 +5,7 @@ namespace Brighty\Cilog\Providers;
 use Brighty\Cilog\Console\InstallCilog;
 use Illuminate\Support\ServiceProvider;
 
-class CilogServiceProvider extends ServiceProvider {
+class CilogLumenServiceProvider extends ServiceProvider {
 
     public function register() {
         $this->app->bind('cilog', function() {
