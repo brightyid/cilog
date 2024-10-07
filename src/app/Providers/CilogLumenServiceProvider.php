ac416f783f658a3f26f629df7bc17183ef55d75c
Egi Aprilianto <contact@asln.dev>
2024-10-07 14:21:06 +0700

    feat: add CilogServiceProvider for binding and command registration

---
+++ src/app/Providers/CilogLumenServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Brighty\Cilog\Providers;
+
+use Brighty\Cilog\Console\InstallCilog;
+use Illuminate\Support\ServiceProvider;
+
+class CilogServiceProvider extends ServiceProvider {
+
+    public function register() {
+        $this->app->bind('cilog', function() {
+            return new Cilog();
+        });
+        
+        $this->mergeConfigFrom(
+            __DIR__.'/../../config/cilog.php', 'cilog'
+        );
+    }
+
+    public function boot() {
+        $this->registerCommands();
+    }
+
+    protected function registerCommands(): void
+    {
+        if (! $this->app->runningInConsole()) {
+            return;
+        }
+
+        $this->commands([
+            InstallCilog::class,
+        ]);
+    }
+}
